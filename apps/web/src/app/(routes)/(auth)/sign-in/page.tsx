6bece0ffba6aee9fa667cae628fe7b357827cdab
Jasper Verbeet <jasperverbeet@gmail.com>
2025-10-11 11:44:14 +0200

    feat(auth): working magic links

--- apps/web/src/app/(routes)/(auth)/sign-in/page.tsx
+++ apps/web/src/app/(routes)/(auth)/sign-in/page.tsx
@@ -1,16 +1,12 @@
 import SignInForm from '@/app/_auth/components/SignInForm';
 import Logo from '@/app/_ui/components/Logo/Logo';
-import Typography from '@/app/_ui/components/Typography/Typography';
 
 const Page = async () => {
   return (
-    <div className="container mx-auto flex w-full max-w-md flex-col items-center gap-8">
+    <div className="container mx-auto flex w-full max-w-sm flex-col items-center gap-8">
       <div>
         <Logo className="h-6 w-auto" />
       </div>
-      <Typography variant="headingMd" className="w-full text-center">
-        What&apos;s your email address?
-      </Typography>
       <SignInForm />
     </div>
   );
