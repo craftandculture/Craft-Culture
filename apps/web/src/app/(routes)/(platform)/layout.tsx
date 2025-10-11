7390a462546a7ac099efdd190ef6784d5735c845
Jasper Verbeet <jasperverbeet@gmail.com>
2025-10-11 18:17:00 +0200

    chore: makes dynamic

--- apps/web/src/app/(routes)/(platform)/layout.tsx
+++ apps/web/src/app/(routes)/(platform)/layout.tsx
@@ -5,6 +5,8 @@ import getQueryClient from '@/lib/react-query';
 import api from '@/lib/trpc/server';
 import tryCatch from '@/utils/tryCatch';
 
+export const dynamic = 'force-dynamic';
+
 const PlatformLayout = async ({ children }: React.PropsWithChildren) => {
   const queryClient = getQueryClient();
 
