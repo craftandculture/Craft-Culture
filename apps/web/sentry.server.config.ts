42ff5826309d25f2cbfd72ffbbbd81969f294dc7
Jasper Verbeet <jasperverbeet@gmail.com>
2025-10-11 10:47:47 +0200

    build: fixes build

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -7,13 +7,12 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://55c873ccc3be9ca5a263cfc9c13984f9@o964095.ingest.us.sentry.io/4510169801883648",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
 });
