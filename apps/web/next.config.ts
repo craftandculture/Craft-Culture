9ea684db0c11db8ec5dc7351c25390d71dd3d80e
Jasper Verbeet <jasperverbeet@gmail.com>
2025-10-11 10:47:47 +0200

    build: fixes build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -41,10 +41,10 @@ export default process.env.NODE_ENV === 'development'
   ? configWithMDX
   : withSentryConfig(configWithMDX, {
       // For all available options, see:
-      // https://github.com/getsentry/sentry-webpack-plugin#options
+      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-      org: 'bvsc-software-bv',
-      project: 'easybooker-web',
+      org: 'byont-ventures',
+      project: 'craft-and-culture',
 
       // Only print logs for uploading source maps in CI
       silent: !process.env.CI,
@@ -55,11 +55,11 @@ export default process.env.NODE_ENV === 'development'
       // Upload a larger set of source maps for prettier stack traces (increases build time)
       widenClientFileUpload: true,
 
-      // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
       // This can increase your server load as well as your hosting bill.
       // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
       // side errors will fail.
-      // tunnelRoute: "/monitoring",
+      tunnelRoute: '/monitoring',
 
       // Automatically tree-shake Sentry logger statements to reduce bundle size
       disableLogger: true,
