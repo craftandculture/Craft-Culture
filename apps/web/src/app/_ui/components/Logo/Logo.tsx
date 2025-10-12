ff64fa1a3d09debd21372ac09e396d5ec0a2cec3
Jasper Verbeet <jasperverbeet@gmail.com>
2025-10-12 19:00:53 +0200

    fix: dark mode fixes and responsive

--- apps/web/src/app/_ui/components/Logo/Logo.tsx
+++ apps/web/src/app/_ui/components/Logo/Logo.tsx
@@ -1,11 +1,22 @@
 import Image, { ImageProps } from 'next/image';
+import { twMerge } from 'tailwind-merge';
 
 import LogoImage from './logo.webp';
 
 export interface LogoProps extends Omit<ImageProps, 'src' | 'alt'> {}
 
-const Logo = ({ ...props }: LogoProps) => {
-  return <Image src={LogoImage} alt="Logo" {...props} />;
+const Logo = ({ className, ...props }: LogoProps) => {
+  return (
+    <Image
+      src={LogoImage}
+      alt="Logo"
+      className={twMerge(
+        'transition-[filter] dark:invert dark:brightness-0 dark:contrast-100',
+        className,
+      )}
+      {...props}
+    />
+  );
 };
 
 export default Logo;
